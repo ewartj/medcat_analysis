55346238452e2dc2b1a2d404314e4cd6663963d4
ewartj <ewart.jonny@gmail.com>
2021-08-27 11:24:54 +0100

    swapped frokl altair to plotly

--- stacked_bar_chart.py
+++ stacked_bar_chart.py
@@ -4,7 +4,7 @@ import altair as alt
 
 alt.renderers.enable('mimetype')
 
-df = pd.read_csv("May_all_mod.csv", index_col="index")
+df = pd.read_csv("Aug_all_resukts.csv", index_col="index")
 df.drop(["counts"], axis = 1, inplace = True)
 df.drop(['address', 'date', "gmc number", "national insurance","url"], axis=0,inplace = True)
 df.reset_index(inplace=True)
@@ -13,21 +13,28 @@ print(df.head())
 df2 = pd.melt(df, id_vars=['index'], value_vars=['Patient','Relative','Healthcare professional'])
 print(df2.head())
 
+import plotly.express as px
+
+# wide_df = px.data.medals_wide()
+
+fig = px.bar(df2, x="value", y="index", color='variable', title="Wide-Form Input")
+fig.show()
+
 # test = df2.to_dict('index')
 
-chart = alt.Chart(df2).mark_bar().transform_calculate(
-    y="split(datum.index, ' ')"
-    ).encode(
-    y=alt.Y('index', axis=alt.Axis(title='')),
-    x=alt.X('sum(value)', axis=alt.Axis(title='total annotations')),
-    color=alt.Color('variable', legend=alt.Legend(orient="top",title="", symbolLimit=2))
-).configure_axis(
-    labelFontSize=20,
-    titleFontSize=20
-).configure_legend(
-titleFontSize=18,
-labelFontSize=18
-) 
+# chart = alt.Chart(df2).mark_bar().transform_calculate(
+#     x="split(datum.index, ' ')"
+#     ).encode(
+#     y=alt.X('sum(value)', axis=alt.Axis(title='total annotations')),
+#     x=alt.Y('index', axis=alt.Axis(title='')),
+#     color=alt.Color('variable', legend=alt.Legend(orient="top",title="", symbolLimit=2))
+# ).configure_axis(
+#     labelFontSize=20,
+#     titleFontSize=20
+# ).configure_legend(
+# titleFontSize=18,
+# labelFontSize=18
+# ) 
 
 # text = chart.mark_text(align='left',baseline='middle', lineBreak='\n').encode(text='df:index')
 # (chart + text).properties(width=500)
@@ -50,4 +57,4 @@ labelFontSize=18
 #     height=300
 # )
 
-chart.show()
+# chart.show()
